f88c3af9fa90c4a28c4fac80d1a32c62ec0a5157
songbingqian <songbingqian_qt@chinamobile.com>
2021-03-05 13:20:05 +0800

    服务提供端8001使用hystrix 超时或运行异常 服务降级

--- cloud-provider-hystrix-payment8001/src/main/java/com/yunfan/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/yunfan/springcloud/service/impl/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.yunfan.springcloud.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.yunfan.springcloud.service.PaymentService;
 import org.springframework.stereotype.Service;
 
@@ -20,13 +22,20 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     @Override
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String paymentInfo_Timeout(Integer id) {
-        int timeNumber = 3;
+        int timeNumber = 5;
         try {
             TimeUnit.SECONDS.sleep(timeNumber);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程池：" + Thread.currentThread().getName() + " paymentInfo_Timeout, id: "+ id+"\t"+"耗时(s): "+timeNumber;
+    }
+
+    public String paymentInfo_TimeoutHandler(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + " paymentInfo_TimeoutHandler, id: "+ id+"\t";
     }
 }
