bf4a8102e081bfd3021c03e33d1832f287017fec
songbingqian <songbingqian_qt@chinamobile.com>
2021-04-20 11:11:13 +0800

    增加网关模块 配置路由规则指定访问payment8001入口

---
+++ cloud-gateway-gateway9527/src/main/java/com/yunfan/springcloud/GateWayMain9527.java
@@ -0,0 +1,19 @@
+package com.yunfan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Administrator
+ * @description
+ * @create 2021-04-20 10:30
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
