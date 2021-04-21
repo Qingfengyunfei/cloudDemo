7cae4363ffce6efada5b7e636f633bdcf4e53d79
songbingqian <songbingqian_qt@chinamobile.com>
2021-04-21 13:34:00 +0800

    配置中心客户端动态刷新[手动调用actuator refresh方法]

--- cloud-config-client3355/src/main/java/com/yunfan/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/yunfan/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.yunfan.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 1.0.0
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.datasource.username}")
