20212487b2b8d4f607fe1015a37eac7c862048b2
songbingqian <songbingqian_qt@chinamobile.com>
2021-04-21 10:12:34 +0800

    增加服务端配置中心模块

---
+++ cloud-config-center3344/src/main/java/com/yunfan/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.yunfan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Administrator
+ * @description
+ * @create 2021-04-21 9:05
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
